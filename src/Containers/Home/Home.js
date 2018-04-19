76d6fa0d97491be6e4c87721078cd16d43b59d33
Matt Mortensen <mortensenm1205@gmail.com>
2018-04-19 15:30:12 -0600

    Updated Home container to have carousel component'

--- src/Containers/Home/Home.js
+++ src/Containers/Home/Home.js
@@ -1,10 +1,38 @@
 import React from "react";
+import { Carousel } from "react-responsive-carousel";
+import styles from "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const Home = props => {
   return (
     <div>
       <h1>{props.page}</h1>
-      <p>Adding a hard coded message</p>
+      <Carousel
+        autoPlay
+        infiniteLoop
+        showArrows={false}
+        showStatus={false}
+        showThumbs={false}
+        showIndicators={false}
+      >
+        <div>
+          <img
+            alt="filler-text"
+            src="http://www.poshreklam.com/wp-content/uploads/2014/06/placeholder.png"
+          />
+        </div>
+        <div>
+          <img
+            alt="filler-text"
+            src="http://gulfinfotech.com/wp-content/uploads/revslider/rev_slider_example/placeholder-blue.png"
+          />
+        </div>
+        <div>
+          <img
+            alt="filler-text"
+            src="http://www.apexcartage.com/wp-content/uploads/revslider/rev_slider_example/placeholder-red.png"
+          />
+        </div>
+      </Carousel>
     </div>
   );
 };
