a1be8612aadf6b4c18126be0c2f3902b958fffbe
Matt Mortensen <mortensenm1205@gmail.com>
2018-04-24 00:00:57 -0600

    got featured image to appear on post card, i think i can now convert this local state to redux

--- src/Components/PostCard.js
+++ src/Components/PostCard.js
@@ -13,7 +13,7 @@ class PostCard extends Component {
   }
 
   componentDidMount() {
-    axios.get('http://reacttranslation.local/wp-json/wp/v2/posts')
+    axios.get('http://reacttranslation.local/wp-json/wp/v2/posts?_embed')
     .then(response => {
       this.setState({
         posts: response.data
@@ -22,15 +22,20 @@ class PostCard extends Component {
   }
 
   render() {
+
     return (
       <div>
       {this.state.posts.map(post => {
         return (
           <Card style={{ width: '35%' }} key={post.id}>
-          <CardMedia>
-            <img src={process.env.PUBLIC_URL + '/images/placeholder_blue.png'} alt='Some text' />
-          </CardMedia>
           <CardTitle title={post.title.rendered} />
+          <CardMedia>
+          {post.better_featured_image ?
+            <img src={post.better_featured_image.source_url} alt={post.better_featured_image.alt_text} />
+            :
+            <img src={process.env.PUBLIC_URL + '/images/placeholder_blue.png'} />
+          }
+          </CardMedia>
           <CardText>{renderHTML(post.content.rendered)}</CardText>
           </Card>
         )
